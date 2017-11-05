087b8e7b82c801e61b15edb924435e2fd5e20db6
ManfredSteyer <manfred.steyer@gmx.net>
2017-11-05 20:03:07 +0100

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'flight-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent  {
+}
